db6768140e6b559b45e8c37c2482feed1f0643d2
jojoe77777 <jojoe77777@users.noreply.github.com>
2026-03-06 17:32:46 +1100

    Add image safety to discord icon

--- src/gui.cpp
+++ src/gui.cpp
@@ -3232,6 +3232,10 @@ void RenderSettingsGUI() {
                                         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
                                         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
                                         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+                                        glPixelStorei(GL_UNPACK_ROW_LENGTH, 0);
+                                        glPixelStorei(GL_UNPACK_SKIP_PIXELS, 0);
+                                        glPixelStorei(GL_UNPACK_SKIP_ROWS, 0);
+                                        glPixelStorei(GL_UNPACK_ALIGNMENT, 4);
                                         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, w, h, 0, GL_RGBA, GL_UNSIGNED_BYTE, pixels);
                                         glBindTexture(GL_TEXTURE_2D, 0);
                                         stbi_image_free(pixels);
